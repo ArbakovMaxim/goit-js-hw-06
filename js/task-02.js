b0afa52deb444b8bb40f9e41cdf1209600c18897
Fefaodessa <fefaodessa@gmail.com>
2022-03-12 13:39:48 +0200

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsEl = ingredients.map(ingredient => {
+
+  const elementListEl = document.createElement(`li`);
+    elementListEl.classList.add(`item`);
+    elementListEl.textContent = ingredient;
+
+  return elementListEl
+})
+
+const listEl = document.querySelector(`#ingredients`);
+
+listEl.append(... ingredientsEl);
