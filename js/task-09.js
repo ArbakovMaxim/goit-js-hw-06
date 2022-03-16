bca951246fbfee3e422b4a1bb1577861a0712f9f
Fefaodessa <fefaodessa@gmail.com>
2022-03-16 20:23:34 +0200

    Test 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  spanColor : document.querySelector(".color"),
+  btn : document.querySelector('.change-color'),
+  body : document.querySelector('body')
+}
+
+refs.btn.addEventListener("click", changeColor);
+
+function changeColor(){
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.spanColor.textContent = refs.body.style.backgroundColor;
+
+}
