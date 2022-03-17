611c0472d32ae6ca7461ef578075735ecb3f0545
Fefaodessa <fefaodessa@gmail.com>
2022-03-17 14:26:48 +0200

    test 10 dop

--- js/task-10.js
+++ js/task-10.js
@@ -4,10 +4,47 @@ function getRandomHexColor() {
 
 
 const refs = {
-  controls : document.querySelector("#controls"),
   imput : document.querySelector("input"),
   createButton : document.querySelector("[data-create]"),
-  destroyButton : document.querySelector("[data-create]"),
+  destroyButton : document.querySelector("[data-destroy]"),
   boxes : document.querySelector("#boxes"),
 }
 
+
+refs.imput.addEventListener("input", onInput);
+
+  function onInput(event){
+    refs.imput.value =  event.currentTarget.value;
+}
+
+
+  const onButton = () => {
+    const amount = refs.imput.value;
+    createBoxes(amount);
+}
+
+refs.createButton.addEventListener("click", onButton);
+
+
+    function createBoxes(amount){
+      const ress = [];
+
+      for (let i = 0 + 1; i <= amount ; i+=1){  
+        ress.push(i);                 
+      } 
+      const allDiv = ress.map(res => {
+        const divEl = document.createElement('div')
+          divEl.style.width = (20  + (res * 10)) + "px";
+          divEl.style.height = (20 + (res * 10)) + "px" ;
+          divEl.style.backgroundColor = getRandomHexColor();
+      
+        return divEl
+      })
+      refs.boxes.append(...allDiv);
+    }
+
+    const destroyBoxes = () => {
+      refs.boxes.innerHTML = '';
+    }
+    
+    refs.destroyButton.addEventListener("click", destroyBoxes);
