c49b3f54af1d47b76dc30515f48a8036539477aa
Fefaodessa <fefaodessa@gmail.com>
2022-03-25 20:47:07 +0200

    Dz 6 Test 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,10 @@
 const input = document.querySelector("#validation-input");
 
 input.addEventListener('blur', inputLength);
+const lengthInput = input.dataset.length
 
 function inputLength (event){
-    if (event.currentTarget.value.length === input.dataset.length){
+    if (event.currentTarget.value.length === Number(lengthInput)){
        return input.classList ='valid';
     }
     input.classList = "invalid";
