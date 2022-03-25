6c7b1b5d83a2bfef766add737a171f73d921557a
Fefaodessa <fefaodessa@gmail.com>
2022-03-25 21:37:48 +0200

    DZ 6 Test 1 fix bag

--- js/task-01.js
+++ js/task-01.js
@@ -3,34 +3,13 @@ const listEl = document.querySelectorAll(`.item`);
 console.log(`Number of categories: `, listEl.length);
 console.log(``);
 
-//Первый списиок
 
-const ferstListEl = document.querySelector(`.item`);
-const ferstTitleEl = ferstListEl.firstElementChild;
-console.log(`Category: ${ferstTitleEl.textContent}`)
-
-const ferstListInsideListEl = document.querySelector(`.item, li`)
-const LastChildInsideListEL = ferstListInsideListEl.lastElementChild;
-const AllChildInsideLastListEl = LastChildInsideListEL.querySelectorAll(`li`);
-console.log(`Elements: ${AllChildInsideLastListEl.length}`);
-console.log(``);
-
-//Второй список
-
-
-const secondChildrenListEl = listEl[1];
-const secondTitleEl = secondChildrenListEl.firstElementChild;
-console.log(`Category: ${secondTitleEl.textContent}`)
-
-const AllChildInsideSecondLastListEl = secondChildrenListEl.querySelectorAll(`li`)
-console.log(`Elements: ${AllChildInsideSecondLastListEl.length}`);
-console.log(``);
-
-//Третий список
-
-const thirdChildrenListEl = listEl[2];
-const thirdTitleEl = thirdChildrenListEl.firstElementChild;
-console.log(`Category: ${thirdTitleEl.textContent}`)
-
-const AllChildInsideThirdLastListEl = thirdChildrenListEl.querySelectorAll(`li`)
-console.log(`Elements: ${AllChildInsideThirdLastListEl.length}`);
+const allList = listEl.forEach( list => {
+    const childrenListEl = list;
+    const titleEl = childrenListEl.firstElementChild;
+    console.log(`Category: ${titleEl.textContent}`)
+    
+    const AllChildInsideSecondLastListEl = childrenListEl.querySelectorAll(`li`)
+    console.log(`Elements: ${AllChildInsideSecondLastListEl.length}`);
+    console.log(``);
+});
